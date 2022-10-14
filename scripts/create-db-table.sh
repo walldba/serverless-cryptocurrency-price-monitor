d33a23720f2999b5cf96f2ae12892fb01854963b
walldba <wallacedba@gmail.com>
2022-10-14 16:05:08 -0300

    refactor: adjust create table script

--- scripts/create-db-table.sh
+++ scripts/create-db-table.sh
@@ -3,10 +3,27 @@ URL=http://localhost:4566
 
 aws dynamodb create-table \
     --attribute-definitions \
-        AttributeName=price,AttributeType=S \
         AttributeName=currency,AttributeType=S \
-    --key-schema AttributeName=currency,KeyType=HASH AttributeName=price,KeyType=RANGE \
+        AttributeName=price,AttributeType=S \
+        AttributeName=createdAt,AttributeType=S \
+    --key-schema AttributeName=currency,KeyType=HASH AttributeName=createdAt,KeyType=RANGE \
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
+    --global-secondary-indexes \
+        "[
+            {
+                \"IndexName\": \"PriceIndex\", 
+                \"KeySchema\": [{\"AttributeName\":\"currency\",\"KeyType\":\"HASH\"},
+                                {\"AttributeName\":\"price\",\"KeyType\":\"RANGE\"}],
+                \"Projection\":{
+                    \"ProjectionType\":\"INCLUDE\",
+                    \"NonKeyAttributes\":[\"createdAt\"]
+                },
+                \"ProvisionedThroughput\": {
+                    \"ReadCapacityUnits\": 1,
+                    \"WriteCapacityUnits\": 1
+                }
+            }
+        ]" \
     --table-name $TABLE_NAME \
     --endpoint-url $URL \
     &>> ./scripts/db_create.txt
