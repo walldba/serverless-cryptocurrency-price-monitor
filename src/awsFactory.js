58c3d6000f7cb70c31e008fa5fb4f9c85f72dbb3
walldba <wallacedba@gmail.com>
2022-10-20 20:23:54 -0300

    feat: add ses on aws factory

--- src/awsFactory.js
+++ src/awsFactory.js
@@ -2,15 +2,13 @@ const AWS = require('aws-sdk');
 
 const isLocal = process.env.IS_LOCAL;
 
-if (isLocal) {
-  AWS.config.update({
-    endpoint: 'http://localhost:4566',
-    credentials: { accessKeyId: 'test', secretAccessKey: 'test' },
-  });
-}
+const DynamoDB = new AWS.DynamoDB.DocumentClient({
+  endpoint: isLocal ? 'http://localhost:4566' : undefined,
+});
 
-const DynamoDB = new AWS.DynamoDB.DocumentClient();
+const SES = new AWS.SES();
 
 module.exports = {
+  SESFactory: SES,
   DynamoDBFactory: DynamoDB,
 };
