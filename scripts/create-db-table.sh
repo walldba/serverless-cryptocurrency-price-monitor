cc67fb2ecca8c1947733b31a88bd6ded00fda4d7
walldba <wallacedba@gmail.com>
2022-10-20 18:16:49 -0300

    chore: add oscillation table on db script

--- scripts/create-db-table.sh
+++ scripts/create-db-table.sh
@@ -1,4 +1,5 @@
-TABLE_NAME=cryptocurrency_prices
+DYNAMODB_PRICES_TABLE=cryptocurrency_prices
+DYNAMODB_OSCILLATIONS_TABLE=cryptocurrency_oscillations
 URL=http://localhost:4566
 
 aws dynamodb create-table \
@@ -24,6 +25,33 @@ aws dynamodb create-table \
                 }
             }
         ]" \
-    --table-name $TABLE_NAME \
+    --table-name $DYNAMODB_PRICES_TABLE \
     --endpoint-url $URL \
-    &>> ./scripts/db_create.txt
+    &>> ./scripts/db_prices_create.txt
+
+aws dynamodb create-table \
+    --attribute-definitions \
+        AttributeName=currency,AttributeType=S \
+        AttributeName=oscillation,AttributeType=S \
+        AttributeName=createdAt,AttributeType=S \
+    --key-schema AttributeName=currency,KeyType=HASH AttributeName=createdAt,KeyType=RANGE \
+    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 \
+    --global-secondary-indexes \
+        "[
+            {
+                \"IndexName\": \"OscillationIndex\", 
+                \"KeySchema\": [{\"AttributeName\":\"currency\",\"KeyType\":\"HASH\"},
+                                {\"AttributeName\":\"oscillation\",\"KeyType\":\"RANGE\"}],
+                \"Projection\":{
+                    \"ProjectionType\":\"INCLUDE\",
+                    \"NonKeyAttributes\":[\"createdAt\"]
+                },
+                \"ProvisionedThroughput\": {
+                    \"ReadCapacityUnits\": 1,
+                    \"WriteCapacityUnits\": 1
+                }
+            }
+        ]" \
+    --table-name $DYNAMODB_OSCILLATIONS_TABLE \
+    --endpoint-url $URL \
+    &>> ./scripts/db_oscillations_create.txt
