a665a9bb21fab9e2f6581e91ecc9d48fb12d9b57
walldba <wallacedba@gmail.com>
2022-10-25 12:15:24 -0300

    chore: add default base url on config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  CRYPTO_BASE_URL: process.env.CRYPTO_BASE_URL,
+  CRYPTO_BASE_URL:
+    process.env.CRYPTO_BASE_URL || 'https://www.mercadobitcoin.net/api/',
   CURRENCY: process.env.CURRENCY || 'BTC',
   PERCENTAGE_VALUE: +process.env.PERCENTAGE_VALUE || 0.01,
   SES_EMAIL_TO: process.env.SES_EMAIL_TO,
